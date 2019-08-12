45bf16fed92323527def84875ffb62de65db40d5
adong666666 <3440217568@qq.com>
2019-08-12 16:34:45 +0800

    更新至swift5，消除所有警告(忽略第三方库警告)

---
+++ TodayNews(AfterMaintenance)/News/Pods/SGPagingView/SGPagingView/SGPagingView.h
@@ -0,0 +1,14 @@
+//
+//  SGPagingView.h
+//  Version 1.6.1
+//  GitHub：https://github.com/kingsic/SGPagingView
+//
+//  Created by kingsic on 2016/10/6.
+//  Copyright © 2016年 kingsic. All rights reserved.
+//
+
+#import "SGPageTitleViewConfigure.h"
+#import "SGPageTitleView.h"
+#import "SGPageContentScrollView.h"
+#import "SGPageContentCollectionView.h"
+#import "SGPagingViewPopGestureVC.h"
